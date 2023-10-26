bc101569c0968ce06775c0046947109f55fa25c0
Jovial Joe Jayarson <jovial7joe@hotmail.com>
2023-10-26 11:19:28 +0530

    wip-fix: sort input, catch len condition

--- python/search_binary.py
+++ python/search_binary.py
@@ -1,6 +1,5 @@
 """Binary Search."""
 
-# Requires Array Sorting with proper Comparator
 
 def binary_search(given_sequence: str, search_term: str):
     """Binary Search Function."""
@@ -9,19 +8,29 @@ def binary_search(given_sequence: str, search_term: str):
     left_cursor = 0
     right_cursor = length_of_given_sequence
 
+    if length_of_search_term > length_of_given_sequence:
+        return False
+    elif length_of_search_term == length_of_given_sequence:
+        return search_term == given_sequence
 
     while right_cursor <= right_cursor:
-        mid_cursor = (right_cursor - left_cursor) // 2
-        if given_sequence[mid_cursor:mid_cursor+length_of_search_term] == search_term:
+        mid_cursor_st = (right_cursor - left_cursor) // 2
+        mid_cursor_en = mid_cursor_st + length_of_search_term
+        exSlice = given_sequence[mid_cursor_st:mid_cursor_en]
+
+        if exSlice == search_term:
             return True
-        left_cursor += 1
-        right_cursor = left_cursor + length_of_search_term
+        elif exSlice[0] > search_term[0]:
+            right_cursor = mid_cursor_st
+        else:
+            left_cursor = mid_cursor_st
     return False
+
 
 def main():
     """Main."""
-    sample_input_sequence = "This is an example sequence."
-    sample_input_search_term = "an ex"
+    sample_input_sequence = "    .Taaceeeeehiilmnnpqsssux"
+    sample_input_search_term = " .Ta"
 
     input_sequence = input("Sequence: ") or sample_input_sequence
     input_search_term = input("Search term: ") or sample_input_search_term
